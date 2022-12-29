3c995525af0a524fd797cba27a86886c72e49170
Kim, Hyeonseo <dodok8@gmail.com>
2022-12-29 21:24:56 +0900

    Add proxy setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  server: {
+    open: true, //개발 서버를 실행할 때 브라우저를 자동으로 엽니다.
+    port: 3000, //3000번 포트로 개발 서버를 실행합니다.
+    proxy: {
+      //CORS 방지를 위한 프록시 설정입니다.
+      "/local": {
+        target: "https://your.api.domain.here", //실제 api 도메인을 넣어줍니다.
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/local/, ""), //target url을 교체합니다.
+      },
+    },
+  },
   plugins: [react()],
-})
+});
