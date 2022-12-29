e260c9f34cae88844518295d35acfc6d56335a0d
Kim, Hyeonseo <dodok8@gmail.com>
2022-12-29 21:27:02 +0900

    Add typescript path setting and vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +19,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
