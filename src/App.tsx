a30a4d412badff9e3008a3627e7bec7656ea6c7c
Kim, Hyeonseo <dodok8@gmail.com>
2023-01-05 22:44:01 +0900

    Add i18n setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 import ReactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
+  const { t } = useTranslation("main");
 
   return (
     <div className="App">
@@ -14,7 +16,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <ReactLogo />
+          <img src="/src/assets/react.svg" className="logo" alt="Vite logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -26,9 +28,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">{t("title")}</p>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import "./locales/index.ts";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
