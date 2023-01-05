a423c0d88be054aced196187a28853a1356583d0
Kim, Hyeonseo <dodok8@gmail.com>
2023-01-05 21:39:51 +0900

    Change type of svg import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import { useState } from "react";
 
-import reactLogo from "./assets/react.svg";
+import ReactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +14,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <ReactLogo />
         </a>
       </div>
       <h1>Vite + React</h1>
