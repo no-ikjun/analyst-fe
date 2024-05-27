fd04f0e552f04d7abc631c8b0d39359e5d9cb7c6
ikjunchoi <choiikjun1101@gmail.com>
2024-05-27 20:43:01 +0900

    feat: stock data search

---
+++ src/components/StockInfo.tsx
@@ -0,0 +1,28 @@
+import "../styles/StockInfo.css";
+
+import axios from "axios";
+
+const StockInfo = ({ stockInfo }: any) => {
+  const handleAddToWatchlist = async () => {
+    try {
+      const response = await axios.post("http://localhost:5050/api/watchlist", {
+        stockCode: stockInfo.code,
+        stockName: stockInfo.name,
+      });
+      console.log("관심 종목에 추가 성공:", response.data);
+    } catch (error) {
+      console.error("관심 종목에 추가 실패:", error);
+    }
+  };
+
+  return (
+    <div className="stock-info-container">
+      <h3>주식 정보</h3>
+      <p>{stockInfo.std_pdno}</p>
+      <p>{stockInfo.prdt_abrv_name}</p>
+      <button onClick={handleAddToWatchlist}>관심 종목에 추가</button>
+    </div>
+  );
+};
+
+export default StockInfo;
